ed7c116b182a226c06f086dc317902938037d714
Jonathan Hefner <jonathan.hefner@gmail.com>
2013-02-23 18:13:20 -0500

    Support signed integers

--- calc.rb
+++ calc.rb
@@ -5,5 +5,19 @@ require './interpreter'
 parser = CalcParser.new
 interp = CalcInterpreter.new
 
-ast = parser.parse('1336 + 1')
-p interp.apply(ast).eval
+input = [
+  '1336 + 1',
+  '+1 + +1',
+  '+1++1',
+  '-1 + 1'
+]
+  
+input.each do |i|
+  ast = parser.parse(i)
+  result = interp.apply(ast).eval
+
+  puts i
+  puts "TREE #{ast}"
+  puts "EVAL #{result}"
+  puts ''
+end
--- parser.rb
+++ parser.rb
@@ -4,8 +4,11 @@ require 'parslet'
 class CalcParser < Parslet::Parser
   rule(:space) { match('\s').repeat(1) }
   rule(:space?) { space.maybe }
+  rule(:sign) { match('[+-]') }
+  rule(:sign?) { sign.maybe }
+  rule(:digit) { match('[0-9]') }
 
-  rule(:integer) { match('[0-9]').repeat(1).as(:int) }
+  rule(:integer) { (sign? >> digit.repeat(1)).as(:int) }
   rule(:operator) { match('[+]') }
   
   rule(:sum) { integer.as(:left) >> space? >> operator.as(:op) >> space? >> expression.as(:right) }
